81b4db2967089b44981edbd6b81be943552f6194
wifijoe <amhammondformal@gmail.com>
2025-02-14 07:16:27 +0000

    init(playwright) adding config files and some tests to code

---
+++ tests/pizza.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('home page', async ({ page }) => {
+  await page.goto('/');
+
+  expect(await page.title()).toBe('JWT Pizza');
+});
+
+test('buy pizza with login', async ({ page }) => {
+    
+});
